b757cc5a6d9dae254f072d4b81cd54b643d61bea
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2021-03-20 10:29:27 +0000

    Lesson 03: DotNet dynamic linking

--- aula01-jvm-interop/App.java
+++ aula01-jvm-interop/App.java
@@ -1,10 +1,18 @@
 public class App {
     public static void demo1() {
-        Point p = new Point(3, 7);
-        System.out.println("Module = " + p.getModule());
+        //Point p = new Point(3, 7);
+        //System.out.println("Module = " + p.getModule());
     }
 
     public static void main(String [] args) {
         System.out.println("Hello World");
+        Teacher t = null;
     }
 }
+
+ 
+interface Person { }
+class PersonBase implements Person { }
+class Student extends PersonBase { }
+class Teacher extends PersonBase { }
+
---
+++ aula03-dotnet-dynamic-linking/PointApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PointLib;
+
+namespace PointApp
+{
+    class Program
+    {
+        public static void demo1() {
+            Point p = new Point(3, 7);
+            Console.WriteLine("Module = " + p.getModule());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            demo1();
+            Teacher teacher;
+
+        }
+    }
+}
+
+
+interface Person { }
+class PersonBase : Person { }
+class Student : PersonBase {}
+class Teacher : PersonBase { }
