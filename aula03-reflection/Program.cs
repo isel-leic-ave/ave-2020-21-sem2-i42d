e54bc5a131890fb668a24b3e3a487289abe3a0fc
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2021-03-20 10:30:12 +0000

    Lesson 03: Reflection

---
+++ aula03-reflection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula03_reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type t = typeof(Program);
+
+            Console.WriteLine(t.Name);
+            Console.WriteLine(t.Namespace);
+            Console.WriteLine(t.BaseType.Name);
+            MethodInfo methods = t.GetMethods();
+
+            foreach(var mi in methods) {
+                Console.WriteLine(mi.Name);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
